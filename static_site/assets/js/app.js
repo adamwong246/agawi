f36671551fae6a3d7631a3430a2c84016891dc10
adamwong246 <adam@spaceport.io>
2013-03-11 18:30:05 -0700

    init javascript

--- static_site/assets/js/app.js
+++ static_site/assets/js/app.js
@@ -1,5 +1,30 @@
+
 $(document).ready(function(){
-  
+  var letters    = ["alpha","beta","gamma", "delta", "epsilon", "zeta"];
+  var def_letter = letters[0];
+
+  function get_detail(){
+    var value = $.url().param('detail');
+
+    if ($.inArray(value, letters) > -1)
+      {
+
+        return value;
+      }
+    else
+      {
+
+        return def_letter;
+      }
+  }; 
+
   $('.carousel').carousel();
+  // var tech = GetURLParameter('detail');
+
+  var detail = get_detail();
+  var detail_class = "." + detail;
+
+  $(detail_class).css({'border-width':'9px', 'border-style':'solid', 'border-color':'red'});
 
 });
+
