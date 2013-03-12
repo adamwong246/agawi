ce3951d4c17a20451a8ff5dfaf741911205e6bea
adamwong246 <adam@spaceport.io>
2013-03-12 10:19:53 -0700

    switching via js works

--- static_site/assets/js/app.js
+++ static_site/assets/js/app.js
@@ -1,9 +1,23 @@
-
 $(document).ready(function(){
+  $('.carousel').carousel();
+
   var letters    = ["alpha","beta","gamma", "delta", "epsilon", "zeta"];
   var def_letter = letters[0];
 
-  function get_detail(){
+  function apply_detail(detail){
+    var detail_class = "." + detail;
+
+    $(".switcher").css({'border':'none'});
+    $(detail_class).css({'border-width':'9px', 'border-style':'solid', 'border-color':'red'});
+
+    $("#dynamic_showcase").attr({
+      src: "assets/"+ detail +".png",
+      title: "jQuery",
+      alt: "jQuery Logo"
+    });
+  };
+
+  function get_detail_param_from_url(){
     var value = $.url().param('detail');
 
     if ($.inArray(value, letters) > -1)
@@ -18,13 +32,15 @@ $(document).ready(function(){
       }
   }; 
 
-  $('.carousel').carousel();
-  // var tech = GetURLParameter('detail');
 
-  var detail = get_detail();
-  var detail_class = "." + detail;
+  var detail = get_detail_param_from_url();
 
-  $(detail_class).css({'border-width':'9px', 'border-style':'solid', 'border-color':'red'});
+  apply_detail(detail);
+
+  $(".switcher").click(function() {
+    selected = $(this).attr('class').split(/\s+/)[0];
+    apply_detail(selected);
+  });
 
 });
 
